d3ea2a044fed27a0aea46d8df041a418b1a6a5ed
e-karov <e_karov@yahoo.com>
2019-05-07 23:17:46 +0200

    Forum-files modified

---
+++ Forum/Forum/Controllers/CommentController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Forum.Data;
+using Forum.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Forum.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly ForumDbContext context;
+
+        public CommentController(ForumDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: Topic/Details/{topicId}/Comment/Create
+        [Authorize]
+        [HttpGet]
+        [Route("/Topic/Details/{id}/Comment/Create")]
+        public IActionResult Create(int id)
+        {
+            return View();
+        }
+
+        //POST: Topic/Details/id/Comment/Create
+        [Authorize]
+        [HttpPost]
+        [Route("/Topic/Details/{TopicId}/Comment/Create")]
+        public IActionResult Create (Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                // Set CreateDate and LastUpdateDate
+                comment.CreatedDate = DateTime.Now;
+                comment.LastUpdatedDate = DateTime.Now;
+
+                string authorId = context
+                    .Users
+                    .Where(u => u.UserName == User.Identity.Name)
+                    .SingleOrDefault()
+                    .Id;
+
+                comment.AuthorId = authorId;
+
+                Topic topic = context.Topics.Find(comment.TopicId);
+                topic.LastUpdatedDate = DateTime.Now;
+
+                context.Comments.Add(comment);
+                context.SaveChanges();
+
+                return Redirect($"/Topic/Details/{comment.TopicId}");
+            }
+
+            return View(comment);
+        }
+
+        //GET: /Topic/Details/{TopicID}/Comment/Edit/{id}
+        [Authorize]
+        [HttpGet]
+        [Route("/Topic/Details/{TopicId}/Comment/Edit/{id}")]
+        public IActionResult Edit(int? topicId, int? id)
+        {
+            if (id == null)
+            {
+                return RedirectPermanent($"/Topic/Details/{ topicId}");
+
+            }
+
+            Comment comment = context
+                .Comments
+                .Include(c => c.Author)
+                .Include(c => c.Topic)
+                .ThenInclude(t => t.Author)
+                .SingleOrDefault(c => c.CommentId == id);
+
+            if (comment == null)
+            {
+                return RedirectPermanent($"/Topic/Details/{topicId}");
+            }
+
+            return View(comment);
+        }
+
+        //POST: /Topic/Details/{topicId}/Comment/Edit/{id}
+        [Route("/Topic/Details/{TopicId}/Comment/Edit/{id}")]
+        public IActionResult Edit(Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                Comment commentToEdit = context
+                    .Comments
+                    .Include(c => c.Author)
+                    .SingleOrDefault(c => c.CommentId.Equals(comment.TopicId));
+
+                if (commentToEdit == null)
+                {
+                    return RedirectPermanent($"/Topic/Details/{comment.TopicId}");
+                }
+
+                commentToEdit.Description = comment.Description;
+                commentToEdit.LastUpdatedDate = DateTime.Now;
+
+                Topic topic = context.Topics.Find(comment.TopicId);
+                topic.LastUpdatedDate = DateTime.Now;
+
+                context.SaveChanges();
+
+                return RedirectPermanent($"/Topic/Details/{comment.TopicId}");
+            }
+            return View(comment);
+        }
+
+        //GET: /Topic/Details/{TopicID}/Comment/Delete/{id}
+        [Authorize]
+        [HttpGet]
+        [Route("/Topic/Details/{TopicId}/Comment/Delete/{id}")]
+        public IActionResult Delete(int topicId, int? id)
+        {
+            if (id == null)
+            {
+                return RedirectPermanent($"/Topic/Details/{topicId}");
+            }
+
+            var comment = context
+                .Comments
+                .Include(c => c.Author)
+                .Include(c => c.Topic)
+                .ThenInclude(t => t.Author)
+                .SingleOrDefault(c => c.CommentId == id);
+
+            if (comment == null)
+            {
+                return RedirectPermanent("/Topic/Details/{topicId}");
+            }
+
+            return View(comment);
+        }
+
+        //POST: /Topic/Details/{TopicId}/Comment/Delete/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("/Topic/Details/{TopicId}/Comment/Delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            var comment = context
+                .Comments
+                .Find(id);
+            if (comment != null)
+            {
+                Topic topic = context.Topics.Find(comment.TopicId);
+                topic.LastUpdatedDate = DateTime.Now;
+
+                context.Comments.Remove(comment);
+                context.SaveChanges();
+            }
+            return RedirectPermanent("/Topic/Details/{comment.TopicId}");
+        }
+    }
+}
---
+++ Forum/Forum/Controllers/TopicController.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Forum.Data;
+using Forum.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Forum.Controllers
+{
+    public class TopicController : Controller
+    {
+        private readonly ForumDbContext context;
+
+        public TopicController (ForumDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Topic/Details/id
+        public IActionResult Details (int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            Topic topic = context.Topics
+                .Include(t => t.Author)
+                .Include(t => t.Comments)
+                .ThenInclude(c => c.Author)
+                .Where(t => t.Id == id)
+                .FirstOrDefault();
+
+            if (topic == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(topic);
+        }
+
+        //GET: Topic/Create
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Topic/Create
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Create (Topic topic)
+        {
+            if (ModelState.IsValid)
+            {
+                                    // Insert topic in DB:
+
+                // - Set CreatedDate and LastUpdadetDate:
+                topic.CreatedDate = DateTime.Now;
+                topic.LastUpdatedDate = DateTime.Now;
+
+                // - Get userId:
+                string authorId = context.Users
+                .Where(u => u.UserName == User.Identity.Name)
+                .First()
+                .Id;
+
+                // - Set topic authorId:
+                topic.AuthorId = authorId;
+
+                // - Save topic:
+                context.Topics.Add(topic);
+                context.SaveChanges();
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View(topic);
+        }
+
+        // Get: Topic/Delete/id
+        [Authorize]
+        public IActionResult Delete (int? id)
+        {
+            if (id == null)
+            {
+                RedirectToAction("Index", "Home");
+            }
+
+            var topic = context.Topics
+                .Include(t => t.Author)
+                .FirstOrDefault(t => t.Id == id);
+
+            if (topic == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(topic);
+        }
+
+        // POST: Topic/Delete/id
+        [HttpPost]
+        [Authorize]
+        public IActionResult Delete(int id)
+        {
+            // get topic:
+            Topic topic = context.Topics
+                .Include(t => t.Author)
+                .FirstOrDefault(t => t.Id == id);
+
+            // check if topic exists:
+            if (topic != null)
+            {
+                // delete topic:
+                context.Topics.Remove(topic);
+                context.SaveChanges();
+            }
+
+            // redirect to Index page:
+            return RedirectToAction("Index", "Home");
+        }
+
+        //GET: Topic/Edti/id:
+
+        public IActionResult Edit (int? id)
+        {
+            // checking if id is null:
+            if (id == null)
+            {
+                RedirectToAction("Index", "Home");
+            }
+
+            // getting topic from DB:
+            Topic topic = context.Topics
+                .Include(t => t.Author)
+                .Where(t => t.Id == id)
+                .SingleOrDefault();
+
+            // checking if the topic exists:
+            if (topic == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            // passing the model to the view:
+            return View(topic);
+        }
+
+        //POST: Topic/Edit/id
+        [HttpPost]
+        [Authorize]
+        public IActionResult Edit (Topic topic)
+        {
+            if (ModelState.IsValid)
+            {
+                Topic topicToEdit = context.Topics
+                    .Include(t => t.Author)
+                    .SingleOrDefault(t => t.Id.Equals(topic.Id));
+
+                if (topicToEdit == null)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                topicToEdit.Title = topic.Title;
+                topicToEdit.Description = topic.Description;
+
+                topicToEdit.LastUpdatedDate = DateTime.Now;
+
+                context.SaveChanges();
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(topic);
+        }
+
+    }
+}
